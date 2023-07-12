a90fc0511612d3aada91d4f85c46f82fa1575db5
Jay Bretherton <jake.bretherton05@gmail.com>
2023-07-12 19:44:06 +0100

    avatar

--- cd.bat
+++ cd.bat
@@ -1,4 +1,4 @@
 cd C:\Users\user\Documents\GitHub\Website\Probably-Jay.github.io\docs
+cmd /k
 start chrome "http://localhost:4000"
 bundle exec jekyll serve
-cmd /k
--- hostOnly.bat
+++ hostOnly.bat
@@ -1,3 +1,2 @@
 cd C:\Users\user\Documents\GitHub\Website\Probably-Jay.github.io\docs
 bundle exec jekyll serve
-cmd /k
